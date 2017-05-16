ae0e29d103f91b038d04c8e8befb3267cc9fa28f
Adam Snow <adamjohnsnow@icloud.com>
2017-05-16 19:18:56 +0100

    asychronicity solved, still query error

--- spec/UserSpec.js
+++ spec/UserSpec.js
@@ -1,26 +1,74 @@
-var UserModel = require('../src/User');
-var mongoose = require('mongoose');
 
-describe('Create an instance of UserModel', function() {
+  var UserModel = require('../src/User');
+  var mongoose = require('mongoose');
 
-  beforeEach(function() {
-    UserModel.remove({}, function(err) {
-      console.log('collection removed');
-    });
-  });
+  describe('Create an instance of UserModel', function() {
 
+    beforeAll(function(done) {
+      UserModel.remove({}, function(err) {
+        console.log('collection removed');
+              done();
+      });
 
-  it('should save to the database', function() {
-    var userAhmed = new UserModel();
-    userAhmed.name = 'Nigel';
-    userAhmed.email = 'nigel@egypt.com';
-    userAhmed.password = '123456';
-    userAhmed.save(function(err) {
-      expect(err).toBeNull();
-    });
-        expect(userAhmed.name).toEqual('Nigel')
     });
 
-    console.log()
 
-  });
+    it('should save to the database', function(done) {
+      var userAhmed = new UserModel();
+      userAhmed.name = 'Nigel';
+      userAhmed.email = 'nigel@egypt.com';
+      userAhmed.password = '123456';
+      userAhmed.save(function(err) {
+        expect(err).toBeNull();
+      });
+
+        done();
+        });
+
+
+      it('bloody better work', function(done) {
+        testSample = UserModel.find(function (err, users) {
+          if (err) return console.error(err);
+          return users;
+        })
+        console.log(testSample)
+        done();
+            });
+    });
+
+
+
+
+// var UserModel = require('../src/User');
+// var mongoose = require('mongoose');
+//
+// describe('Create an instance of UserModel', function() {
+//
+//   // beforeEach(function() {
+//   //   UserModel.remove({}, function(err) {
+//   //     console.log('collection removed');
+//   //   });
+//   // });
+//
+//
+//   it('should save to the database', function() {
+//     var testName;
+//     var userAhmed = new UserModel();
+//     userAhmed.name = 'Nigel';
+//     userAhmed.email = 'nigel@egypt.com';
+//     userAhmed.password = '123456';
+//     userAhmed.save(function(err) {
+//       expect(err).toBeNull();
+//     });
+//
+// function testName() {
+//   UserModel.find(function (err, users) {
+//   if (err) return console.error(err);
+//   return users[0].name;
+// })
+//
+//     console.log(testName())
+//     expect(testName()).toEqual('Nigel')
+//     });
+//
+//   });
