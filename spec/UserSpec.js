34aaeb8abd5d4e62bd3594347475fc27cd2a65b8
Adam Snow <adamjohnsnow@icloud.com>
2017-05-16 16:16:05 +0100

    npm dependencies fixed

--- spec/UserSpec.js
+++ spec/UserSpec.js
@@ -1,14 +1,13 @@
 var UserModel = require('../src/User');
 var mongoose = require('mongoose');
 
-
 describe('Create an instance of UserModel', function() {
 
-  // afterEach(function() {
-  //   UserModel.remove({}, function(err) {
-  //     console.log('collection removed');
-  //   });
-  // });
+  beforeEach(function() {
+    UserModel.remove({}, function(err) {
+      console.log('collection removed');
+    });
+  });
 
 
   it('should save to the database', function() {
@@ -18,15 +17,10 @@ describe('Create an instance of UserModel', function() {
     userAhmed.password = '123456';
     userAhmed.save(function(err) {
       expect(err).toBeNull();
-
+    });
+        expect(userAhmed.name).toEqual('Nigel')
     });
 
-    it('jdjd', function() {
-      //UserModel.find(function(err, result) {
-      console.log(UserModel.findOne({'name': 'Nigel'}))
-        expect(db.usermodels.findOne({'name': 'Nigel'})).toBe('Nenn')
-        //expect(result.name).toBe('Ben');
+    console.log()
 
-    });
   });
-});
