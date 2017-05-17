1a9650a9e5b5462f8cfc2b332b9d281068931f9b
Adam Snow <adamjohnsnow@icloud.com>
2017-05-17 13:22:00 +0100

    session user and homepage

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ var app = express();
 var User = require('./src/User');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var sess;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(session({
-  user: {test: "test"},
   secret: 'keyboard cat',
-  resave: false,
+  resave: true,
   saveUninitialized: true,
-  cookie: { secure: true }
 }));
 
 app.set('view engine', 'ejs');
@@ -21,19 +20,15 @@ app.get('/', function (req, res) {
 });
 
 app.post('/signup', function(req, res) {
-  var user = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.passwordConfirmation
-  };
-  req.session = user.name
+  sess = req.session
+  sess.name = req.body.name;
   res.redirect('/home')
+
 });
 
 app.get('/home', function(req, res) {
-  console.log(req.session)
-  res.send(req.session)
+sess = req.session
+res.render('pages/home');
 });
 
 app.listen(3000, function () {
