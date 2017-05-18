cecf98f611146ed05e600f069e8b54b4f1f341a0
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-18 11:36:15 +0100

    uses zombie to test signup page and redirection

--- test/signupSpec.js
+++ test/signupSpec.js
@@ -1,24 +1,35 @@
 const Browser = require('zombie');
+var UserModel = require('../src/User');
+var mongoose = require('mongoose');
 var app = require('../app.js')
 var expect = require('chai').expect
 var assert = require('assert');
 Browser.localhost('localhost', 3000);
 
 describe('User visits signup page', function() {
-
   const browser = new Browser();
-
   before(function(done) {
     browser.visit('/', done);
   });
 
   describe('submits form', function(){
-
     it('should have a signup page', function(){
       browser.assert.text('h1','Feel at home, anywhere');
     });
+    it('should submit signup form', function(done){
+      browser
+      .fill('name', 'testUser')
+      .fill('email', 'test@test.com')
+      .fill('password', 'tron21')
+      .fill('passwordConfirmation', 'tron21')
+      .pressButton('Submit').then(function() {
+        assert.ok(browser.success);
+        browser.assert.text('p', 'Welcome back to RocksBnBtestUser')
+      }).then(done, done);
+    });
   });
 });
+
 
 
 describe('Array', function() {
