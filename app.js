3b461cde6fecac92c04591b5ccd98ced576546b4
Adam Snow <adamjohnsnow@icloud.com>
2017-05-18 12:02:11 +0100

    heroku package startup

--- app.js
+++ app.js
@@ -52,6 +52,8 @@ name: sess.name
 });
 
 app.use(express.static(__dirname + '/views/pages'));
-app.listen(3000, function () {
+
+var port = process.env.PORT || 3000
+app.listen(port, function () {
   console.log(app.settings.env)
 });
